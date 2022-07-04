8704be926f59333cdb55992eea54809da81581f9
FedruciDev <fedset2004@gmail.com>
2022-07-04 16:07:35 +0200

    backup

--- api.php
+++ api.php
@@ -1,10 +1,7 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
 include ("method/db.php");
-include("method/login.php");
-include("method/user.php");
-include("method/permission.php");
-include("method/product.php");
+include ("method/permission.php");
 include("method/Response/response.php");
 include("method/Response/success_response.php");
 include("method/Response/failed_response.php");
@@ -14,243 +11,15 @@ try
     
     if (!empty($_POST) && !empty($_POST["action"]) )
     {
-        $perms=new Permission();
-        $action=$_POST["action"];
-        if (!empty($_POST["auth"]))
+        if (!empty($_POST["token"]))
         {
-            $authValue=json_decode($_POST["auth"]);
-            $authVerify=new Login();
-            $authVerify=$authVerify->authverify($authValue->email,$authValue->token);
-            
-            if ($authVerify->ok==True)
+            $token=$_POST["token"];
+            $auth=new Permission();
+            if ($auth->verify_token($token)->ok==true)
             {
-                if ($action=="usersGet")
+                $action=$_POST["action"];
+                if ($action="get_traffic")
                 {
-                    $user=new User();
-                    $users=$user->allUserGet();
-                    if ($users->ok==true)
-                    {
-                        $response=new Success();
-                        $response->data=$users->result;
-                        $response->Send(); 
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-                } 
-                if ($action=="getAllRoles")
-                {
-                    
-                    $roles=$perms->getAllRoles();
-                    if ($roles->ok==true)
-                    {
-                        $response=new Success();
-                        $response->data=$roles->result;
-                        $response->Send(); 
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-                }    
-                else if ($action=="usersDelete")
-                {
-                    $user=new User();
-                    
-                    $result=$user->usersDelete(json_decode($_POST["value"]));
-                    if($result->ok==true)
-                    {
-                        $response=new Success();
-                        $response->Send();
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-
-                }
-                else if ($action=="getAllTable")
-                {
-                        $result=$perms->getAllTable();
-                        $response=new Success();
-                        $response->data=$result->result;
-                        $response->Send();
-                   
-
-                }
-                else if ($action=="rolesDelete")
-                {
-                    
-                    
-                    $result=$perms->rolesDelete(json_decode($_POST["value"]));
-                    if($result->ok==true)
-                    {
-                        $response=new Success();
-                        $response->Send();
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-
-                }
-                else if ($action=="userAdd")
-                {
-
-                    $user=new User();
-                    
-                    $result=$user->userAdd(($_POST["value"]));
-                    if($result->ok==true)
-                    {
-                        $response=new Success();
-                        $response->Send();
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-
-                }
-                else if ($action=="rolesAdd")
-                {
-
-                    
-                    
-                    $result=$perms->rolesAdd(($_POST["value"]));
-                    if($result->ok==true)
-                    {
-                        $response=new Success();
-                        $response->Send();
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-
-                }
-                else if ($action=="getUserRoles")
-                {
-                    $user=new User();
-                    $result=$user->getUserRoles();
-                    if($result->ok==true)
-                    {
-                        $response=new Success();
-                        $response->data=$result->result;
-                        $response->Send();
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-
-                }
-
-                //-------------
-                //prodotti
-                //-------------
-                
-                if ($action=="getAllProduct")
-                {
-                    
-                    $product=new Product();
-                    $products=$product->getAllProduct();
-                    if ($products->ok==true)
-                    {
-                        $response=new Success();
-                        $response->data=$products->result;
-                        $response->Send(); 
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->Send();
-                    }
-                } 
-
-            }
-
-            else
-            {
-                $response=new Failed();
-                $response->message="Permission denied";
-                $response->Send();
-            }
-        
-        }
-        else
-        {
-            if ($_POST["action"]=="login")
-            {
-                if (!empty($_POST["values"]))
-                    {
-                        $values=json_decode($_POST["values"]);
-                        $password=$values->password;
-                        $email=$values->email;
-                        $login=new Login();
-                        $check=$login->login($email,$password);
-                        
-                        if ($check->ok==True)
-                        {
-                            
-                            $response=new Success();
-                            $response->data=$check->result;
-                            $response->Send();
-                        
-                        }
-                        else
-                        {
-                            $response=new Failed();
-                            $response->message="Invalid credential";
-                            $response->Send();
-                        }
-                    }
-                    else
-                    {
-                        
-                    }
-            }  
-        
-            if ($_POST["action"]=="authverify")
-            {
-                if (!empty($_POST["values"]))
-                {
-                    $values=json_decode($_POST["values"]);
-                    if (isset($values->token) && isset($values->email))
-                    {
-                        $token=$values->token;
-                        $email=$values->email;
-                        $login=new Login();
-                        $check=$login->authverify($email,$token);
-        
-                        if ($check->ok==True)
-                        {
-                            $response=new Success();
-                            $response->data=$check->result;
-                            $response->message="Valid token";
-                            $response->Send();
-        
-                        }
-                        else
-                        {
-                            $response=new Failed();
-                            $response->message="Invalid token";
-                            $response->Send();
-                        }
-                    }
-                    else
-                    {
-                        $response=new Failed();
-                        $response->message="Invalid token";
-                        $response->Send();
-                    }
                     
                 }
                 else
@@ -258,55 +27,10 @@ try
                     $response=new Failed();
                     $response->Send();
                 }
-            } 
-                    
-            if ($_POST["action"]=="logout")
-            {
-                if (!empty($_POST["values"]))
-                {
-                    $values=json_decode($_POST["values"]);
-                    if (isset($values->token) && isset($values->email))
-                    {
-                        $token=$values->token;
-                        $email=$values->email;
-                        $login=new Login();
-                        $result=$login->logout($email,$token);
-                        if ($result->ok==True)
-                        {
-                            $response=new Success();
-                            $response->data->email=$email;
-                            $response->message="Valid token";
-                            $response->Send();
-                        }
-                    }
-                }
-        
             }
-        
-            if ($_POST["action"]=="path_permission")
-            {
-                
-                
-                $path_perm=$perms->PathPermission();
-                $response=new Success();
-                $response->data=$path_perm->result;
-                $response->Send();
-        
-            }
-        }
-    
-
-        
             
 
-
-        
-
-    }
-    else
-    {
-        $response=new Failed();
-        $response->Send();
+        }
     }
 } catch(Exception $e){
     $response=new Failed();
